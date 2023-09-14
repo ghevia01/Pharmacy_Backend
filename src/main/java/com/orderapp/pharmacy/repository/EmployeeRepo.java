21544e39e472447e031a9f2835c2af351f8c4dc7
cmorenointer <cmoreno3775@interbayamon.edu>
2023-09-14 15:47:53 -0400

    removes unnused models, repos & services

--- src/main/java/com/orderapp/pharmacy/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.orderapp.pharmacy.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.orderapp.pharmacy.data.model.Employee;
-
-public interface EmployeeRepo extends MongoRepository<Employee, String> {
-
-}
--- src/main/java/com/orderapp/pharmacy/security/jwt/JwtUtils.java
+++ src/main/java/com/orderapp/pharmacy/security/jwt/JwtUtils.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
